11189e5b24b82504eb11d0f9244d0269f9b1076a
Billy Ponto <bpgponto@gmail.com>
2022-04-15 08:27:52 +0800

    save

---
+++ assets/script/tailwind-config.js
@@ -0,0 +1,23 @@
+tailwind.config = {
+    theme: {
+        // container: {
+        //     center: true,
+        //     padding: {
+        //         DEFAULT: '1rem',
+        //         md: '2rem',
+        //         lg: '50px',
+        //         xl: '120px'
+        //     }
+        // },
+        fontFamily: {
+            poppins: 'Poppins, sans-serif'
+        },
+        extend: {
+            colors: {
+                "alerange": '#FB6908',
+                "gray-1": '#B4B4B4',
+                "gray-2": '#E2E0E0',
+            }
+        }
+    }
+}
