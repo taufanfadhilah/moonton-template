eb4ae6a83cb67f4196f69dcb8a2fa7363e43a38f
Billy Ponto <bpgponto@gmail.com>
2022-04-16 13:35:04 +0800

    update sign in page, dashboard margin

--- assets/script/tailwind-config.js
+++ assets/script/tailwind-config.js
@@ -9,6 +9,10 @@ tailwind.config = {
                 "gray-1": '#B4B4B4',
                 "gray-2": '#E2E0E0',
                 "form-bg": '#212121',
+            },
+            screens: {
+                'laptopLg': '1160px',
+                'laptopXl': '1360px',
             }
         }
     }
